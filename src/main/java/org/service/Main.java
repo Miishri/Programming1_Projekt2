fa518a5c0faab16bee71949a43c1bc129684c671
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-08-28 18:07:37 +0200

    complete bus lights text-art

--- src/main/java/org/service/Main.java
+++ src/main/java/org/service/Main.java
@@ -1,7 +1,33 @@
 package org.service;
 
+import java.util.Arrays;
+
 public class Main {
+    
+    final double adultTicket = 299.90;
+    final double juvenileTicket = 149.90;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] myNumbers = {
+                {"1","2","3","4"},
+                {"5","6","7","8"},
+                {"9","10","11","12"},
+                {"13","14","15","16"},
+                {"17","18","19","20"}
+        };
+
+        printBussSeats(myNumbers);
+    }
+
+    private static void printBussSeats(String[][] bussSeats) {
+        System.out.println("Night bus towards Japan.\n Available seats =");
+        System.out.printf("   %3s%n", "\\-/   \\-/");
+        System.out.print("___>-<___>-<___");
+        System.out.print("\n|_|______  _===:");
+        for (int i = 0; i < bussSeats.length; i++) {
+            for (int j = 0; j < 4; j++) {
+
+            }
+        }
     }
 }
