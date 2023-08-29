c76b8d5d08ec00bd5c8a966b35e5767ed0503769
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-08-29 19:44:50 +0200

    implement customer details

--- src/main/java/org/service/Main.java
+++ src/main/java/org/service/Main.java
@@ -1,43 +1,142 @@
 package org.service;
 
-public class Main {
-    
-    final double adultTicket = 299.90;
-    final double juvenileTicket = 149.90;
+import java.time.Year;
+import java.util.Scanner;
 
+public class Main {
+    static final double adultBusTicket = 299.90;
+    static final double juvenileBusTicket = 149.90;
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        String[][] myNumbers = {
+        String[][] bussSeats = {
                 {"1","2","3","4"},
                 {"5","6","7","8"},
                 {"9","10","11","12"},
                 {"13","14","15","16"},
                 {"17","18","19","20"}
         };
+        startBusService(bussSeats);
 
-        printBus(myNumbers);
     }
 
-    private static void printBus(String[][] bussSeats) {
-        printBusFront(bussSeats);
+    public static void startBusService(String[][] bussSeats) {
+        printBus(bussSeats);
+    }
+    public static void inputValidator(int choice) {
+        switch (choice) {
+            case 1:
+            case 2:
+
+        }
+    }
+
+    public static int customerBusSeatChoice() {
+        while (true) {
+            System.out.println("0. Exit");
+            System.out.println("1. Book seat");
+            System.out.println("2. Find your booking ");
+
+            int choice = scanner.nextInt();
+        }
+    }
+    public static void busWindowSeatChoice(int choice) {
+        if (choice == 1) {
+            System.out.println("Window seats unbooked are: " );
+            System.out.println("1. Which seat to book? (1-20)");
+        }
+    }
+
+    public static void unbookedWindowSeats(String[][] busSeats) {
+
+    }
+
+    public static int validateRole() {
+        while (true) {
+            System.out.println("1.Bus Inspector ");
+            System.out.println("2.Customer ");
+
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                return 1;
+            }else if (choice == 2) {
+                return 2;
+            }
+        }
+    }
+
+    public static void bookedSeatDetails(String firstName, String lastName, String dateOfBirth, int busNumber) {
+
+    }
+    public static int checkAvailableSeats(String[][] bussSeats) {
+        int availableSeats = 0;
+        for (int i = 0; i < bussSeats.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (!splittedString(bussSeats[i][j])[0].equals("X")) {
+                    availableSeats += 1;
+                }
+            }
+        }
+
+        return availableSeats;
+    }
+    public static String bookedSeatCheck(String seat) {
+        return splittedString(seat)[0];
+    }
+    public static String getFullName(String seat) {
+        return splittedString(seat)[1] + " " + splittedString(seat)[2];
+    }
+    public static boolean currentAgeCheck(String seat) {
+        int dateOfBirth = Integer.parseInt(splittedString(seat)[3].split("-")[2]);
+        int currentYear = Year.now().getValue();
+        return currentYear - dateOfBirth >= 18;
+    }
+    public static int getSeat(String seat) {
+        return Integer.parseInt(splittedString(seat)[4]);
+    }
+    public static String formatDateOfBirth(String dateOfBirth) {
+        return dateOfBirth.substring(0, 4) + "-" +
+                dateOfBirth.substring(4, 6) + "-" +
+                dateOfBirth.substring(6, 8);
+    }
+    public static double getTotalProfit(String[][] bookedSeats, int rows, int columns, double profit) {
+        if (rows > bookedSeats.length) {
+            return profit;
+        }
+
+
+        if (!currentAgeCheck(bookedSeats[rows][columns])) {
+            return getTotalProfit(bookedSeats, rows + 1, columns + 1, profit + juvenileBusTicket);
+        }
+
+        return getTotalProfit(bookedSeats, rows + 1, columns + 1, profit + adultBusTicket);
+    }
+
+    public static String[] splittedString(String seat) {
+        return seat.split(",");
+    }
+
+    public static void printBus(String[][] bussSeats) {
+        printBusFrontFrame(bussSeats);
+        printBusInteriorLayout(bussSeats);
+        printBusBackFrame();
+    }
+    public static void printBusInteriorLayout(String[][] bussSeats) {
         for (int i = 0; i < bussSeats.length; i++) {
             System.out.println();
             for (int j = 0; j < 4; j++) {
-                printBusSeats(bussSeats[i][j]);
+                printFormattedBusSeats(bussSeats[i][j]);
             }
-            if (i == 1) {
-                System.out.print("\n<<------------>>");
-            }else if (i == 3) {
-                System.out.print("\n<<------------>>");
-            }
+            printWheels(i);
         }
-        printBusBack();
     }
-
-    private static void printBusSeats(String seat) {
-        String bussSeat = String.format("|%2s|", seat);
-        System.out.print(bussSeat);
+    public static void printFormattedBusSeats(String seat) {
+        if (seat.length() <= 2 && !seat.isEmpty()) {
+            System.out.printf("|%2s|", seat);
+        }else {
+            System.out.printf("|%2s", bookedSeatCheck(seat));
+        }
     }
-    private static void printBusFront(String[][] bussSeats){
+    public static void printBusFrontFrame(String[][] bussSeats){
         System.out.println(
                 "Night bus towards Japan." +
                 "\n" +
@@ -47,22 +146,16 @@ public class Main {
         System.out.print("\n/___|----------\\");
         System.out.print("\n|_D_|__/   _===:");
     }
-    private static void printBusBack(){
+    public static void printWheels(int seat) {
+        if (seat == 0) {
+            System.out.print("\n()------------()");
+        }else if (seat == 3) {
+            System.out.print("\n()------------()");
+        }
+    }
+    public static void printBusBackFrame(){
         System.out.println("\n|--------------|");
         System.out.println("\\--------------/");
         System.out.println(" \\-/_\\----/_\\-/");
-    }
-
-    private static int checkAvailableSeats(String[][] bussSeats) {
-        int sum = 0;
-        for (int i = 0; i < bussSeats.length; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (!bussSeats[i][j].equals("X")) {
-                    sum += 1;
-                }
-            }
-        }
-
-        return sum;
     }
 }
